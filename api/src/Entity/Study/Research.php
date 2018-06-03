63301a2c75f6c7451831b947c8338677dc47b20c
roman-curse <frspm.roman@gmail.com>
2018-06-03 13:11:43 +0300

    Add 1st entity

---
+++ api/src/Entity/Study/Research.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by roman
+ * at 03.06.18 12:59
+ * as a part of researcher-api
+ */
+
+namespace App\Entity\Study;
+
+class Research
+{
+    // TODO default data processing
+
+    const TITLE_DEFAULT = 'Research';
+
+}
